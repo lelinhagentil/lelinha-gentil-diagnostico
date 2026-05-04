dcd5af8835c4a8b507446a1d59336330063c12bb
lelinhagentil <lelinhagentil@yahoo.com.br>
2026-05-04 13:47:00 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Facilitador from './Facilitador.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const path = window.location.pathname;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    {path === '/facilitador' ? <Facilitador /> : <App />}
+  </React.StrictMode>,
+)
